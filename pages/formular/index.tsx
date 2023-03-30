319466b286e686852029e45cb365758b17f3c65c
Pjavel <zexyp01@gmail.com>
2023-03-30 10:00:57 +0200

    form added

---
+++ pages/formular/index.tsx
@@ -0,0 +1,43 @@
+import {ChangeEvent, useState} from "react";
+import styles from './styles.module.css';
+
+export default function Index() {
+    let [name, setName] = useState<string>("");
+    let [email, setEmail] = useState<string>("");
+    let [phone, setPhone] = useState<string>("");
+
+    let [nameValid, setNameValid] = useState<boolean>(false);
+    let [emailValid, setEmailValid] = useState<boolean>(false);
+    let [phoneValid, setPhoneValid] = useState<boolean>(false);
+
+    function validateName(e: ChangeEvent<HTMLInputElement>|null) {
+        setNameValid(!!name)
+    }
+
+    function validateEmail(e: ChangeEvent<HTMLInputElement>|null) {
+        setEmailValid(email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) != null)
+    }
+
+    function validatePhone(e: ChangeEvent<HTMLInputElement>|null) {
+        setPhoneValid(phone.match(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/) != null)
+    }
+
+    return (
+        <div>
+            <h1>Crentials Harvestor</h1>
+            <label htmlFor="name">Name</label>
+            <br/>
+            <input className={emailValid ? "" : styles.suspicious} id="name" value={name} onChange={e => { setName(e.target.value); validateName(e); }}/>
+            <br/>
+            <label htmlFor="email">Email</label>
+            <br/>
+            <input className={emailValid ? "" : styles.suspicious} id="email" value={email} onChange={e => { setEmail(e.target.value); validateEmail(e); }}/>
+            <br/>
+            <label htmlFor="phone">Phone</label>
+            <br/>
+            <input className={phoneValid ? "" : styles.suspicious} id="phone" value={phone} onChange={e => { setPhone(e.target.value); validatePhone(e); }}/>
+            <br/>
+            <button disabled={!nameValid || !emailValid || !phoneValid}>Submit</button>
+        </div>
+    );
+}
